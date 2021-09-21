802ee4dd62ee6c979fc49583d69ff305be0418a7
Krishna Naidoo <krishna@krish-macbook.local>
2021-09-21 16:42:17 +0200

    update

--- filetools/gadget/ascii.py
+++ filetools/gadget/ascii.py
@@ -43,6 +43,7 @@ def gadget2ascii(gfname, infoname, ncpu=4):
     nparts = np.zeros(len(blocks))
     nparts[1:] = data[7][:-1]
     nparts = nparts.astype('int')
+    nparts = np.cumsum(nparts)
     fnames = []
     for i in range(0, len(blocks)):
         fnames.append(gfname + '.' + str(i))
@@ -52,23 +53,7 @@ def gadget2ascii(gfname, infoname, ncpu=4):
     outs = [pool.apply(gadget2ascii_single, args=(fnames[i], nparts[i])) for i in range(0, len(blocks))]
     # Step 3: Don't forget to close
     pool.close()
-
-    # Old function
-    # reader = read.ReadGADGET()
-    # startid = 0
-    # for i in range(0, blocks):
-    #     posvel = reader.readsnap(gfname + '.'+str(i), return_pos=True, return_vel=True, part='dm', single=1)
-    #     pos = posvel[0]
-    #     vel = posvel[1]
-    #     x, y, z = pos[:, 0], pos[:, 1], pos[:, 2]
-    #     vx, vy, vz = vel[:, 0], vel[:, 1], vel[:, 2]
-    #     #partid = np.arange(len(x)) + startid
-    #     #startid += len(x)
-    #     partid = np.arange(len(x))
-    #     if i > 0:
-    #         partid += nparts[i-1]
-    #     np.savetxt(gfname + '_' + str(i) + '.ascii', np.column_stack([x, y, z, vx, vy, vz, partid]))
-
+    
 
 def rm_gadget_ascii_copy(gfname):
     """Removes all ascii copies of the gadget file.
